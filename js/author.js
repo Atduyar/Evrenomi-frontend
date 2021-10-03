d31f8254aead4897929c964e02501449a5b26e54
Atduyar <atduyar@gmail.com>
2021-10-03 22:38:04 +0300

    tamam

--- js/author.js
+++ js/author.js
@@ -28,7 +28,7 @@ function getUserBlogs(userId) {
             setUserBlogs(b);
         }
         apiUser.resultErrFunction = apiUser.resultErrFunction;
-        apiUser.GetAuth("blogs/getBlogDraft?id="+userId);//+"&pageId="+userBlogsPageId+"&pageSize=42", t.token);
+        apiUser.GetAuth("blogs/getBlogDraft?id="+userId, t.token);//+"&pageId="+userBlogsPageId+"&pageSize=42", t.token);
     }
     apiUser.resultErrFunction = (t) => {
         console.log(t);
