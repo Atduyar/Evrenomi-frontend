c826d25185f0c3f5ed4eefcb83b4254f8eae5890
Atduyar <atduyar@gmail.com>
2021-10-04 18:24:55 +0300

    tamam

--- js/write.js
+++ js/write.js
@@ -113,7 +113,7 @@ function addElement(tagName, data="", des="", lock=false, menu=true){
             return getDefualtElement(`<a contenteditable class="textbox" spellcheck="false" placeholder="Type url...">${data}</a>`, "a", lock, menu)
             break;
         case "b":
-            return getDefualtElement(`<a href="evrenomi.com/blogView/${data+"-"+des}">evrenomi.com/blogView/</a><a contenteditable class="textbox" spellcheck="false" placeholder="Type url..." onkeyup="this.parentElement.children[0].href = this.textContent">${data}-${des}</a>`, "b", lock, menu)
+            return getDefualtElement(`evrenomi.com/blogView/<a contenteditable class="textbox" spellcheck="false" placeholder="Type something...">${data}</a><a contenteditable class="textbox" spellcheck="false" placeholder="Type blog id...">${des}</a>`, "b", lock, menu)
             break;
         case "li":
         case "lı":
