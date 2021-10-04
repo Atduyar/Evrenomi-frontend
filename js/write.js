30b901e31acffe249bf544d5bac1b2dcb26352ee
Atduyar <atduyar@gmail.com>
2021-10-04 18:11:21 +0300

    tamam

--- js/write.js
+++ js/write.js
@@ -113,7 +113,7 @@ function addElement(tagName, data="", des="", lock=false, menu=true){
             return getDefualtElement(`<a contenteditable class="textbox" spellcheck="false" placeholder="Type url...">${data}</a>`, "a", lock, menu)
             break;
         case "b":
-            return getDefualtElement(`evrenomi.com/blogView/<a contenteditable class="textbox" spellcheck="false" placeholder="Type url...">${data}</a>`, "b", lock, menu)
+            return getDefualtElement(`evrenomi.com/blogView/<a contenteditable class="textbox" spellcheck="false" placeholder="Type url...">${data}-${des}</a>`, "b", lock, menu)
             break;
         case "li":
         case "lı":
@@ -227,8 +227,11 @@ function getItemJson(item){
             newItemDescription = "Url";
             break;
         case "b":
-            newItemData = item.getElementsByClassName("textbox")[0].textContent;
-            newItemDescription = "Url";
+            var temp = item.getElementsByClassName("textbox")[0].textContent.split("-");
+
+            newItemData = temp[temp.length-1];
+            temp.pop();
+            newItemDescription = temp.join('-');
             break;
         case "hr":
             // newItemData = "";
