32ba66f4e59b4a66c0f223b53dca7c525b6adac4
Atduyar <atduyar@gmail.com>
2021-10-06 21:41:39 +0300

    tamam

--- js/author.js
+++ js/author.js
@@ -56,7 +56,7 @@ function setUserBlogs(b){
         }
         userBlogsPage.innerHTML += 
         `<div class="${((i%14) >= 12)?"blog-list-long ":""}blog-item">
-            <div class="delete-btn" onclick="deleteBlog(${b[i].blogId},${b[i].blogTitle})">X</div>
+            <div class="delete-btn" onclick="deleteBlog(${b[i].blogId},"${b[i].blogTitle}")">X</div>
             <a href="/write/${b[i].blogId}-${b[i].authorName.includes("Yayında") ? "p" :"t"}" class="a-img-blog-item" >
                 <img class="img-blog-item" onerror="this.src='https://api.atduyar.com/ConstImage/errorImg.jpg';" src="${b[i].blogTitlePhotoUrl == null ? "" : b[i].blogTitlePhotoUrl}" loading="lazy" alt="${b[i].blogTitle}">
             </a>
