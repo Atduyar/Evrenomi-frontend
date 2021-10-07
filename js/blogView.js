2343652fc5cd6aefad023b3c1143aafb57858644
Atduyar <atduyar@gmail.com>
2021-10-07 16:58:16 +0300

    tamam

--- js/blogView.js
+++ js/blogView.js
@@ -116,6 +116,7 @@ function getBlogDetail(id, fixUrl = ()=>{}){
         apiBlogDetail.resultFunction = (b)=>{
             console.log(b);
             fixUrl(b.blogTitle);
+            document.getElementById("div-icerik").classList.remove("fake");
             setBlogDetail(b);
         }
         apiBlogDetail.GetAuth("blogs/getBlogGuest?id="+id);
