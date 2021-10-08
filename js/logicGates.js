8c8f9690dfec1c7603dc4ef93f7dfadad71608fa
Atduyar <atduyar@gmail.com>
2021-10-08 21:57:19 +0300

    tamam

--- js/logicGates.js
+++ js/logicGates.js
@@ -35,11 +35,11 @@ function windowResized() {
 }
 
 class Node{
-	constructor(x,y,r,h=true){
+	constructor(x,y,r,p=Direction.Orijn){
     	this.x=x;
         this.y=y;
         this.r=r;
-        this.h=h;//horizontal
+        this.p=p;//horizontal
     }
     move(x,y){
     	this.x=x;
@@ -52,13 +52,40 @@ class Node{
         circle(this.x, this.y, this.r);
     }
 }
+// 0→  1↓  2←  3↑
+const Direction = {
+    Right = 0,
+    Down = 1,
+    Left = 2,
+    Up = 3,
+    Horizontal = 4,
+    Vertical = 5,
+    Orijn = 6,
+    flip = (x)=>{
+        switch(x){
+            case 0:
+                return 2;
+                break;
+            case 1:
+                return 3;
+                break;
+            case 2:
+                return 0;
+                break;
+            case 3:
+                return 1;
+                break;
+            default:
+                return x;
+        }
+    }
+};
 
 class Gate{
 	constructor(x,y,p=0){
     	this.x=x;
         this.y=y;
         this.p=p;// 0→  1↓  2←  3↑
-        this.h=(p%2==0);// horizontal
     }
     move(x,y){
         throw new Error("Gate implemense edilmemis");
@@ -71,9 +98,9 @@ class Gate{
 class And extends Gate{
 	constructor(x,y,p=0){
         super(x,y,p);
-        this.in1 = new Node(x-25,y+8,10,super.h);
-        this.in2 = new Node(x-25,y-8,10,super.h);
-        this.out1 = new Node(x+25,y,10,super.h);
+        this.in1 = new Node(x-25,y+8,10,Direction.flip(super.p));
+        this.in2 = new Node(x-25,y-8,10,Direction.flip(super.p));
+        this.out1 = new Node(x+25,y,10,super.p);
     }
     move(x,y){
         this.x = x;
@@ -104,11 +131,11 @@ class Wire{
         this.color=passiveWire;
     }
     draw(){
-        drawWire(this.color,this.from.x, this.from.y, this.to.x, this.to.y, this.from.h, this.to.h, true);
+        drawWire(this.color,this.from.x, this.from.y, this.to.x, this.to.y, this.from.p, this.to.p, true);
     }
 }
 
-function drawWire(c,x1,y1,x2,y2,h1=true,h2=true,re=true){
+function drawWire(c,x1,y1,x2,y2,h1=0,h2=0,re=true){
     h = x1 - x2;
     w = y1 - y2;
     noFill();
@@ -120,25 +147,96 @@ function drawWire(c,x1,y1,x2,y2,h1=true,h2=true,re=true){
         strokeWeight(3);
         stroke(c);
     }
+
     beginShape();
-    if(h1){
-        curveVertex(x1+h, y1-w);
+    switch(h1) {
+        case 0:
+            curveVertex(x1-(abs(h)*2+150), y1+w);
+            break;
+        case 1:
+            curveVertex(x1+h, y1-(abs(w)*2+150));
+            break;
+        case 2:
+            curveVertex(x1+(abs(h)*2+150), y1+w);
+            break;
+        case 3:
+            curveVertex(x1+h, y1+(abs(w)*2+150));
+            break;
+        case 4://horizontal
+            curveVertex(x1-h, y1+w);
+            break;
+        case 5://vertical
+            curveVertex(x1+h, y1-w);
+            break;
+        case 6://vertical
+            curveVertex(x1, y1);
+            break;
     }
-    else{
-        curveVertex(x1-h, y1+w);
-    }
+    
     curveVertex(x1, y1);
-    
     curveVertex(x2, y2);
-    if(h2){
-        curveVertex(x2-h, y2+w);
+
+    switch(h2) {
+        case 0:
+            curveVertex(x2-(abs(h)*2+150), y2+w);
+            break;
+        case 1:
+            curveVertex(x2+h, y2-(abs(w)*2+150));
+            break;
+        case 2:
+            curveVertex(x2+(abs(h)*2+150), y2+w);
+            break;
+        case 3:
+            curveVertex(x2+h, y2+(abs(w)*2+150));
+            break;
+        case 4://horizontal
+            curveVertex(x2-h, y2+w);
+            break;
+        case 5://vertical
+            curveVertex(x2+h, y2-w);
+            break;
+        case 6://orijn
+            curveVertex(x2, y2);
+            break;
     }
-    else{
-        curveVertex(x2+h, y2-w);
-    }
+
     endShape();
-    
     if(re){
         drawWire(c,x1,y1,x2,y2,h1,h2,false);
     }
 }
+
+// function drawWire(c,x1,y1,x2,y2,h1=true,h2=true,re=true){
+//     h = x1 - x2;
+//     w = y1 - y2;
+//     noFill();
+//     if(re){
+//         strokeWeight(5);
+//         stroke(0, 0, 0);
+//     }
+//     else{
+//         strokeWeight(3);
+//         stroke(c);
+//     }
+//     beginShape();
+//     if(h1){
+//         curveVertex(x1+h, y1-w);
+//     }
+//     else{
+//         curveVertex(x1-h, y1+w);
+//     }
+//     curveVertex(x1, y1);
+    
+//     curveVertex(x2, y2);
+//     if(h2){
+//         curveVertex(x2-h, y2+w);
+//     }
+//     else{
+//         curveVertex(x2+h, y2-w);
+//     }
+//     endShape();
+    
+//     if(re){
+//         drawWire(c,x1,y1,x2,y2,h1,h2,false);
+//     }
+// }
