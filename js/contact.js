70bdcc86b8aff51d279c5ee0a1c7836f2a944af5
Atduyar <atduyar@gmail.com>
2022-02-16 21:43:36 +0300

    tamam

--- js/contact.js
+++ js/contact.js
@@ -26,18 +26,11 @@ function sendContact(){
     
 }
 
-function InvalidMsg(textbox) {
-    if (textbox.value == '') {
-        textbox.setCustomValidity('Lütfen işaretli yerleri doldurunuz');
-    }
-    else if (textbox.validity.typeMismatch){
-        textbox.setCustomValidity('Lütfen işaretli yere geçerli bir email adresi yazınız.');
-    }
-    else {
-       textbox.setCustomValidity('');
-    }
-    return true;
+document.getElementById("contact-submit").onsubmit = (e)=>{
+    e.preventDefault();
+    sendContact();
 }
+
 alertify.alert("This is an alert dialog.", function(){
     alertify.message("Su anda bu bölümü yapım asamasında 1 gün içerisinde yeniden calısır olucak. Lütfen daha sonra tekrar deneyin.");
 });
